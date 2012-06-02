2c9b726f07fe9082936668b6c67ae3477dcb2102
Lars Meinke <lars.meinke@hhu.de>
2012-06-02 13:12:42 +0200

    modified Tuer

--- src/bomberman/Tuere.java
+++ src/bomberman/Tuere.java
@@ -7,62 +7,33 @@ import javax.swing.JPanel;
 
 public class Tuere extends JFrame {
 	private static final long serialVersionUID = -2181712976539020219L;
-	private JPanel jPanel;
-	private BomberMan b;
 	private JLabel win1 = new JLabel(new ImageIcon("src/gfx/win3.gif"));
 	private JLabel win2 = new JLabel(new ImageIcon("src/gfx/explode.gif"));
-	private JLabel exit = new JLabel(new ImageIcon("src/gfx/door/door.png"));
-	private int x = 400, y = 280;
-	private Spielfeld s;
-	private Bombe bo;
+	private JPanel jPanel;
+	private JLabel[] toRemove;
 
-	public Tuere(JPanel jPanel, BomberMan b, Spielfeld s, Bombe bo) {
+	public Tuere(JPanel jPanel, JLabel[] toRemove) {
 		this.jPanel = jPanel;
-		this.b = b;
-		this.s = s;
-		this.bo = bo;
-		exit.setBounds(x, y, 40, 40);
-		exit.setVisible(true);
-		jPanel.add(exit);
-		
+		this.toRemove = toRemove;
 	}
 
-	public int getX() {
-		return x;
-	}
+	/*
+	 * private boolean isExit() { if(b.getX() == x && b.getY() == y) { return
+	 * true; } return false; }
+	 */
 
-	public int getY() {
-		return y;
-	}
-
-	private boolean isExit() {
-		if(b.getX() == x && b.getY() == y) {
-			return true;
-		}
-		return false;
-	}
-	
 	public void end() {
-		if(isExit()) {
-			JLabel w[][] = s.getWalls(); 
-			for (int i = 0; i < 22; i++) {
-				for (int j = 0; j < 15; j++) {
-					jPanel.remove(w[i][j]);
-				}
-			}
-			
-			jPanel.remove(exit);
-			jPanel.remove(b.getBomberMan());
-			jPanel.remove(bo.getBomb());
-			
-			jPanel.add(win1);
-			win1.setBounds(1,1,840,600);
-			win1.setVisible(true);
-			jPanel.add(win2);
-			win2.setBounds(225, 10, 390, 196);
-			win2.setVisible(true);
-			
+		for( JLabel r : toRemove ) {
+			jPanel.remove(r);
 		}
+		
+		jPanel.add(win1);
+		win1.setBounds(1, 1, 840, 600);
+		win1.setVisible(true);
+		
+		jPanel.add(win2);
+		win2.setBounds(225, 10, 390, 196);
+		win2.setVisible(true);
 	}
 
 }
